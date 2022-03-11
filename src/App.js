85f19caf616c32c8b64c5d36fd71492597b2120e
DikshaYadav22 <diksha.yadav022@gmail.com>
2022-03-11 15:44:48 +0530

    added pagination and random button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import Review from "./components/Review";
-import data from "./components/data";
+
 import React, { useState } from "react";
 
 function App() {
-  const [reviews, setReviews] = useState(data);
-  console.log(reviews);
-
   return (
     <div className="container mx-auto bg-dark text-white">
       <h2 className="text-center p-3">Our Reviews</h2>
-      <Review reviews={reviews} />
+      <Review />
     </div>
   );
 }
--- src/components/Review.js
+++ src/components/Review.js
@@ -1,4 +1,4 @@
-import React, { memo } from "react";
+import React, { memo, useState } from "react";
 import {
   Card,
   CardImg,
@@ -9,47 +9,96 @@ import {
   Button,
 } from "reactstrap";
 
-const Review = memo(({ reviews }) => {
+import data from "./data";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
+
+const Review = memo(() => {
+  const [index, setIndex] = useState(0);
+  const [reviews, setReviews] = useState(data);
+  const { id, image, job, name, text } = reviews[index];
+
+  const checkNumber = (number) => {
+    if (number > reviews.length - 1) {
+      return 0;
+    }
+
+    if (number < 0) {
+      return reviews.length - 1;
+    }
+    return number;
+  };
+
+  const prevPerson = () => {
+    setIndex((index) => {
+      let newIndex = index - 1;
+      return checkNumber(newIndex);
+    });
+  };
+
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return checkNumber(newIndex);
+    });
+  };
+
+  const randomNumber = (number) => {
+    let randomNumber = Math.floor(Math.random() * reviews.length);
+    if (randomNumber === index) {
+      return index + 1;
+    }
+
+    setIndex(checkNumber(randomNumber));
+  };
+
   return (
     <div>
-      {reviews.map((review) => {
-        const { id, image, job, name, text } = review;
-        return (
-          <>
-            <Card className="text-black">
-              <div
-                style={{
-                  width: "30%",
-                  height: "300px",
-                  marginLeft: "360px",
-                }}
-              >
-                <CardImg
-                  style={{ borderRadius: "50%" }}
-                  top
-                  width="100%"
-                  height="100%"
-                  src={image}
-                  alt="Card image cap"
-                />
-              </div>
+      <Card className="text-black">
+        <div
+          style={{
+            width: "30%",
+            height: "300px",
+            marginLeft: "360px",
+          }}
+        >
+          <CardImg
+            style={{ borderRadius: "50%" }}
+            top
+            width="100%"
+            height="100%"
+            src={image}
+            alt="Card image cap"
+          />
+        </div>
 
-              <CardBody className="text-center">
-                <CardTitle>
-                  <h4 className="text-capitalize">{name}</h4>
-                </CardTitle>
-                <CardSubtitle className="text-primary font-weight-normal">
-                  {job}
-                </CardSubtitle>
-                <CardText>
-                  <p className="text-justify">{text}</p>
-                </CardText>
-                <Button color="btn btn-outline-primary">Surprise Me</Button>
-              </CardBody>
-            </Card>
-          </>
-        );
-      })}
+        <CardBody className="text-center">
+          <CardTitle>
+            <h4 className="text-capitalize">{name}</h4>
+          </CardTitle>
+          <CardSubtitle className="text-primary font-weight-normal">
+            {job}
+          </CardSubtitle>
+          <CardText>
+            <p className="text-justify">{text}</p>
+          </CardText>
+          <div>
+            <Button color="light" onClick={prevPerson}>
+              <FontAwesomeIcon icon={faChevronLeft} />
+            </Button>
+            <Button color="light" onClick={nextPerson}>
+              <FontAwesomeIcon icon={faChevronRight} />
+            </Button>
+          </div>
+
+          <Button color="btn btn-outline-primary" onClick={randomNumber}>
+            Surprise Me
+          </Button>
+        </CardBody>
+      </Card>
     </div>
   );
 });
