3e55f1f71f49ca11b347b8716e5b23f616cf1bb2
DikshaYadav22 <diksha.yadav022@gmail.com>
2022-03-11 11:21:12 +0530

    added review and data components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Review from "./components/Review";
+import data from "./components/data";
+import React, { useState } from "react";
 
 function App() {
+  const [reviews, setReviews] = useState(data);
+  console.log(reviews);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto bg-dark text-white">
+      <h2 className="text-center p-3">Our Reviews</h2>
+      <Review reviews={reviews} />
     </div>
   );
 }
--- src/components/Review.js
+++ src/components/Review.js
@@ -1,7 +1,57 @@
 import React, { memo } from "react";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button,
+} from "reactstrap";
 
-const Review = memo(() => {
-  return <div>Review</div>;
+const Review = memo(({ reviews }) => {
+  return (
+    <div>
+      {reviews.map((review) => {
+        const { id, image, job, name, text } = review;
+        return (
+          <>
+            <Card className="text-black">
+              <div
+                style={{
+                  width: "30%",
+                  height: "300px",
+                  marginLeft: "360px",
+                }}
+              >
+                <CardImg
+                  style={{ borderRadius: "50%" }}
+                  top
+                  width="100%"
+                  height="100%"
+                  src={image}
+                  alt="Card image cap"
+                />
+              </div>
+
+              <CardBody className="text-center">
+                <CardTitle>
+                  <h4 className="text-capitalize">{name}</h4>
+                </CardTitle>
+                <CardSubtitle className="text-primary font-weight-normal">
+                  {job}
+                </CardSubtitle>
+                <CardText>
+                  <p className="text-justify">{text}</p>
+                </CardText>
+                <Button color="btn btn-outline-primary">Surprise Me</Button>
+              </CardBody>
+            </Card>
+          </>
+        );
+      })}
+    </div>
+  );
 });
 
 export default Review;
